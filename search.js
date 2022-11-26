21ee42888306fad7dbe747eb96128632e779714c
whitehackteo <whitehackteo@gmail.com>
2022-11-26 00:30:05 -0500

    update

--- search.js
+++ search.js
@@ -3,7 +3,7 @@
 let movieNameRef = document.getElementById("movie-name");
 let searshBtn = document.getElementById("searsh-button");
 let result = document.getElementById("result");
-//Function to fetch data from API
+
 let getMovie = () => {
   let movieName = movieNameRef.value;
   let url = `https://api.themoviedb.org/3/search/movie?api_key=192e0b9821564f26f52949758ea3c473&page=${page}&query=${movieName}`;
@@ -11,12 +11,12 @@ let getMovie = () => {
   if (movieName.length <= 0) {
     result.innerHTML = `<h3 class="msg"></h3>`;
   }
-  //If input field is NOT empty
+ 
   else {
     fetch(url)
       .then((resp) => resp.json())
       .then((data) => {
-        //If movie exists in database
+        
         if (data.Response == "True") {
           result.innerHTML = `
 					<div class="pelicula">
@@ -26,12 +26,12 @@ let getMovie = () => {
 					</div>
 				`;
         }
-        //If movie does NOT exists in database
+        
         else {
           result.innerHTML = `<h3 class='msg'>${data.Error}</h3>`;
         }
       })
-      //If error occurs
+      
       .catch(() => {
         result.innerHTML = `<h3 class="msg">Error Occured</h3>`;
       });
